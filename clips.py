a11d0a563e03c827acc291bc935d9f7c8e719ae0
Gprime <gprime@hotmail.co.uk>
2018-10-05 09:16:40 +0100

    Cleanup

--- clips.py
+++ clips.py
@@ -20,16 +20,13 @@ accounts = dict(u.split(",") for u in config["DEFAULT"]["accounts"].split("\n"))
 opts = {}
 
 def update_users(account_names):
-    """
-    
-    Updates the cache file of usernames and user ids
+    """Updates the cache file of usernames and user ids
     
     Parameters:
      account_names: username or list of usernames
     
     Returns:
      None
-    
     """
     
     if opts.get("nocache"):
@@ -54,16 +51,13 @@ def update_users(account_names):
             config.write(fp)
 
 def get_time(video_id):
-    """
-    
-    Uses the api to get the time the video was created.
+    """Uses the api to get the time the video was created.
     
     Parameters:
-     video_id: if of the VOD.
+     video_id: id of the VOD.
      
     Returns:
      datetime: when the VOD was created.
-    
     """
     
     logging.info(f"Getting video time: {video_id}")
@@ -85,19 +79,16 @@ def parse_offset(time):
     )
 
 def search_videos(name, time, buffer):
-    """
-    
-    Searches the user's past broadcasts for a video that was recorded 
+    """Searches the user's past broadcasts for a video that was recorded 
     during a specific time.
     
     Parameters:
-     name: case insensitive username of the user to search for videos.
-     time: datetime to search for.
+     name:   case insensitive username of the user to search for videos.
+     time:   datetime to search for.
      buffer: amount of time in seconds to take away from the offset.
      
     Returns:
      string: url of VOD with offset or "Not found".
-    
     """
     
     logging.info(f"Searching: name={name} time={time} buffer={buffer}")
@@ -130,18 +121,15 @@ def search_videos(name, time, buffer):
         parameters["after"] = response["pagination"]["cursor"]
 
 def get_videos(clip_url, account_names, buffer=0):
-    """
-    
-    Get a list of videos that were recorded at the time clip_url was created.
+    """Get a list of videos that were recorded at the time clip_url was created.
     
     Parameters:
-     clip_url: full of clip or VOD url with offset
+     clip_url:      full clip or VOD url with offset.
      account_names: username or list of usernames to search for videos.
-     buffer: amount of time in seconds to take away from the offset.
+     buffer:        amount of time in seconds to take away from the offset.
      
     Returns:
      list of (username, url) tuples: If a video does not exist, url is "Not found".
-    
     """
     
     if isinstance(account_names, str):
@@ -150,16 +138,13 @@ def get_videos(clip_url, account_names, buffer=0):
     update_users(account_names)
     
     if clip_url.startswith("https://clips.twitch.tv/"):
-        """
-        
-        The old v2 API is used to get the time and offset that the clip 
+        """The old v2 API is used to get the time and offset that the clip 
         was created.
         
         The new API (https://api.twitch.tv/helix/clips) does not provide 
         the offset (yet 04/10/18).
         
         The VOD offset time cannot be inferred from the created_at field.
-        
         """
         
         url = f"https://clips.twitch.tv/api/v2/clips/{clip_url[24:]}"
@@ -174,7 +159,7 @@ def get_videos(clip_url, account_names, buffer=0):
     elif clip_url.startswith("https://www.twitch.tv/videos/"):
         time = get_time(re.search(r"\d+", clip_url).group())
         
-        if "?t=" in clip_url:
+        if "t=" in clip_url:
             time += parse_offset(clip_url.split("=")[1])
         
         return [(name, search_videos(name, time, buffer)) for name in account_names]
@@ -197,10 +182,7 @@ def parse_args():
     args = parser.parse_args()
     
     opts["nocache"] = args.nocache
-    if args.verbose:
-        logging.getLogger().setLevel(logging.INFO)
-    else:
-        logging.getLogger().setLevel(logging.WARNING)
+    logging.getLogger().setLevel([logging.WARNING, logging.INFO][args.verbose])
     
     return (args.url, args.users, args.buffer)
 
